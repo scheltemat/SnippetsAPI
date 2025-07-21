cfbbcf1d281fac23d0eeb5b09fa028283fcf4995
Scheltema <thomas.scheltema@hp.com>
2025-07-21 15:30:15 -0500

    add all CRUD endpoints

--- Controllers/SnippetsController.cs
+++ Controllers/SnippetsController.cs
@@ -21,5 +21,40 @@ namespace SnippetsAPI.Controllers
             var snippets = _context.Snippets.ToList();
             return Ok(snippets);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSnippet(int id)
+        {
+            var snippet = _context.Snippets.Find(id);
+            if (snippet == null)
+            {
+                return NotFound();
+            }
+            return Ok(snippet);
+        }
+
+        [HttpPost]
+        public IActionResult CreateSnippet(Snippet snippet)
+        {
+            _context.Snippets.Add(snippet);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetSnippet), new { id = snippet.Id }, snippet);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSnippet(int id)
+        {
+            var snippet = _context.Snippets.Find(id);
+            if (snippet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Snippets.Remove(snippet);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
